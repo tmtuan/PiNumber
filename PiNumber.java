9c8d4fec52d0ac6581da2dbc2e2817474810524b
tmtuan <tranminhtuan684@gmail.com>
2017-12-18 23:07:29 +0700

    Method to find a digit range from n-th to m-th after the decimal number of Pi

--- PiNumber.java
+++ PiNumber.java
@@ -40,6 +40,46 @@ class PiNumber {
        }
         return i;
     }
+    
+    /*
+     * Find the series of digits from n-th to m-th after the decimal point of Pi
+     */
+    public static BigInteger digitRange(int n_th, int m_th, String pinumber ) throws IOException {
+       
+        FileInputStream input = null;
+        BigInteger bd = new BigInteger("0");
+        BigInteger base = new BigInteger("10");
+        
+        try {
+            input = new FileInputStream(pinumber);
+            
+            int c;
+            int index = 1;
+            while((c = input.read()) != -1 && index < n_th - 1) {
+                index++;
+            } 
+            while ((c = input.read()) != -1) {
+                if(index < m_th) {
+                    bd = bd.multiply(base);
+                    bd = bd.add(new BigInteger(String.valueOf(c-48))); 
+                    index++;
+                } else {
+                    break;
+                }
+                
+            }
+        }  catch (IOException e) {
+            System.out.println("ERROR: " + e.toString());
+        } catch (Exception e) {
+            System.out.println("ERROR: " + e.toString());
+        }
+        finally {
+           if (input != null) {
+               input.close();
+           }
+       }
+       return bd;
+    }
 
     public static void main(String[] args) throws IOException {
         FileInputStream in = null;
@@ -66,6 +106,10 @@ class PiNumber {
         if (args.length == 2) {
             int i = PiNumber.digitAt(Integer.parseInt(String.valueOf(args[0])), args[1]);
             System.out.println("\nDigit at " + args[0] + "-th of Pi is " + String.valueOf(i));
+        }
+        else if (args.length == 3){
+            BigInteger digit_range = PiNumber.digitRange(Integer.parseInt(String.valueOf(args[0])), Integer.parseInt(String.valueOf(args[1])), args[2]);
+            System.out.println("\nDigits from " + args[0] + "-th to " + args[1] + "-th is: " + digit_range.toString());
         }
         else {
             System.out.println("\n Please rerun with syntax : java PiNumber nth Pi-filename.txt");
